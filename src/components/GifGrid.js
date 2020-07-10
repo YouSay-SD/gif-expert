39ca79a591d5c937024abceaf53bc62b91006a56
YouSay-SD <yousay.sd@gmail.com>
2020-07-10 19:19:19 -0300

    Create Fetch GetGifs

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const GifGrid = ({ category }) => {
+
+  useEffect(() => {
+    getGifs();
+  }, [])
+
+  // Get Gifs
+  const getGifs = async () => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=shinobu&limit=10&api_key=ftdnawdwqdCoVNJcNw2zxOOaAqNo67Ty`;
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+    const gifs = data.map( img => {
+      return {
+        id: img.id,
+        title: img.title,
+        url: img.images?.downsized_medium.url
+      }
+    })
+  }
+
   return (
     <div>
-      <h3>{ category }</h3>
+      { /*
+        gifs.map(( id, title, url) => {
+          return (
+            <img 
+              key={ id }
+              alt={ title }
+              src={ url }
+            />
+          )
+        })*/
+      }
     </div>
   )
 }
