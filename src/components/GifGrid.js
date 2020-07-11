f538d45ec78eea5f02d46d7d8dcb1bea120fd829
YouSay-SD <yousay.sd@gmail.com>
2020-07-10 22:48:04 -0300

    Finish useFetchGifs Hook

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,20 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import GifGridItem from './GifGridItem';
-import getGifs from '../helpers/getGifs';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 const GifGrid = ({ category }) => {
 
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    getGifs( category )
-      .then( imgs => setImages( imgs ))
-  }, [ category ])
-
+  const { data : images, loading } = useFetchGifs( category );
+ 
   return (
     <>
       <h3>{ category }</h3>
 
+      { loading && <span>Loading</span> }
+    
       <div className="card-grid">
 
         {
